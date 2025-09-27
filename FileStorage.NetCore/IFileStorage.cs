507eb39916463acccfd48b330eb74b221c3f083b
Masoud.Sarafraz <Masoud.Sarafraz@MASOUD-PC>
2025-09-27 11:09:18 +0330

    Change

--- FileStorage.NetCore/IFileStorage.cs
+++ FileStorage.NetCore/IFileStorage.cs
@@ -11,7 +11,7 @@ public interface IFileStorage : IDisposable
     Task<Guid> SaveFileAsync(Stream FileStream, CancellationToken CancellationToken = default);
     Task<Guid> SaveFileAsync(byte[] FileData, CancellationToken CancellationToken = default);
     Task<Guid> SaveFileAsync(string Base64, CancellationToken CancellationToken = default);
-    Stream GetFile(Guid fileId);
+    Stream GetFile(Guid FileId);
     Task<Stream> GetFileAsync(Guid FileId, CancellationToken CancellationToken = default);
     byte[] GetFileBytes(Guid FileId);
     Task<byte[]> GetFileBytesAsync(Guid FileId, CancellationToken CancellationToken = default);
